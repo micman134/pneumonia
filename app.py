71ed2ccf2b90bc945c71f3edae513a0f2503fa37
Michael Okoro <michaelokoro1@gmail.com>
2024-02-10 11:40:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,32 +4,39 @@ from PIL import Image
 import tensorflow as tf
 from tensorflow import keras
 
-# Load the trained model
-model = keras.models.load_model('Pneumonia.h5')
+# Upload image through Streamlit
+    uploaded_file = st.file_uploader("Choose a test image...", type=["jpg", "jpeg", "png"])
 
-# Define class labels
-class_labels = ['Normal', 'Pneumonia']
+    # Model performance analysis
+    if uploaded_file is not None:
+       
 
-# Streamlit App
-st.title("Pneumonia Detection App")
+        # Display the uploaded image for training
+        st.image(uploaded_file, caption="Uploaded Image (Training)", use_column_width=True)
 
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg",'png','jpeg'])
+        # Load and preprocess the test image
+        st.write("Processing the image...")
 
-if uploaded_file is not None:
-    # Display the uploaded image
-    image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    
+        # Process the image and perform inference
+        test_image = image.load_img(uploaded_file, target_size=(224, 224))  # Change target size to (224, 224)
+        st.image(test_image, caption="Processed Image (Training)", use_column_width=True)
 
-    # Preprocess the image
-    img_array = np.array(image.resize((224, 224)))
-    img_array = img_array / 255.0  # Normalize pixel values to between 0 and 1
-    img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis=0)
+        test_image = test_image / 255.0  # Normalize
 
-    # Make prediction
-    prediction = model.predict(img_array)
+        # Perform inference for prediction
+        st.write("Performing inference...")
 
-    # Display prediction result
-    predicted_class = class_labels[np.argmax(prediction)]
-    confidence = round(np.max(prediction) * 100, 2)
-    st.write(f"Prediction: {predicted_class}")
-    st.write(f"Confidence: {confidence}%")
+        predictions = model.predict(test_image)
+
+        # Display probability scores for each class
+        st.write("Class Probabilities:")
+        display_class_probabilities(predictions)
+
+        # Print the classification label with probability
+        predicted_class_index = np.argmax(predictions)
+        predicted_class_label = class_labels[predicted_class_index]
+        predicted_class_probability = predictions[0][predicted_class_index] * 100
+        st.success(f'Predicted Class: {predicted_class_label} with {predicted_class_probability:.2f}% probability')
