d29646759f8891176e1b801967e604a8f7209831
Michael Okoro <michaelokoro1@gmail.com>
2024-02-11 10:17:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -94,10 +94,10 @@ elif page == "Processed Pixels":
         st.image(uploaded_file, caption="Uploaded Image (Training)", use_column_width=True)
 
         # Load and preprocess the test image
-        st.write("Processing the image...")
+        #st.write("Processing the image...")
         
         test_image = image.load_img(uploaded_file, target_size=(150, 150))
-        st.image(test_image, caption="Processed Image (Training)", use_column_width=True)
+        #st.image(test_image, caption="Processed Image (Training)", use_column_width=True)
         
         test_image = image.img_to_array(test_image)
         test_image = np.expand_dims(test_image, axis=0)
